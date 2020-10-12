7e778eaef1ab093514587ef9f0023bec0a15d8c6
99sparshm <99.sparsh@gmail.com>
2020-10-12 11:55:29 +0530

    added basic toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "./Layout.module.css";
 
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+
 const layout = (props) => {
     return (
         <React.Fragment>
-            <div>Toolbar, sidebar, backdrop</div>
+            <Toolbar />
             <main className={styles.Content}>{props.children}</main>
         </React.Fragment>
     );
