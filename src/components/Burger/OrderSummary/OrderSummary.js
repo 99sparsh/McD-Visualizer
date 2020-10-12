1752a2b15ab1e369be8cd75040b8e2dd189eae58
99sparshm <99.sparsh@gmail.com>
2020-10-12 11:45:44 +0530

    order modal complete

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Button from "../../UI/Button/Button";
+
 const orderSummary = (props) => {
     const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
         return (
@@ -10,12 +12,22 @@ const orderSummary = (props) => {
             </li>
         );
     });
+
     return (
         <React.Fragment>
             <h3>Your Order</h3>
             <p>A delicious burger with the following ingredients: </p>
             <ul>{ingredientSummary}</ul>
             <p>Continue to Checkout?</p>
+            <p>
+                <strong>Total Price: {props.price}</strong>
+            </p>
+            <Button btnType="Danger" clicked={props.purchaseCancelled}>
+                CANCEL
+            </Button>
+            <Button btnType="Success" clicked={props.purchaseContinued}>
+                CONTINUE
+            </Button>
         </React.Fragment>
     );
 };
