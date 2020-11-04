5f119e6c707cec11ccde9b557623ec08dfda22b1
99sparshm <99.sparsh@gmail.com>
2020-11-04 11:29:03 +0530

    added order form dynamic component

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,24 +1,65 @@
 import React from "react";
 
-import styles from "./Input.module.css";
+import classes from "./Input.module.css";
 
 const input = (props) => {
     let inputElement = null;
-    switch (props.inputtype) {
+    const inputClasses = [classes.InputElement];
+
+    if (props.invalid && props.shouldValidate && props.touched) {
+        inputClasses.push(classes.Invalid);
+    }
+
+    switch (props.elementType) {
         case "input":
-            inputElement = <input className={styles.InputElement} {...props} />;
+            inputElement = (
+                <input
+                    className={inputClasses.join(" ")}
+                    {...props.elementConfig}
+                    value={props.value}
+                    onChange={props.changed}
+                />
+            );
             break;
         case "textarea":
             inputElement = (
-                <textarea className={styles.InputElement} {...props} />
+                <textarea
+                    className={inputClasses.join(" ")}
+                    {...props.elementConfig}
+                    value={props.value}
+                    onChange={props.changed}
+                />
+            );
+            break;
+        case "select":
+            inputElement = (
+                <select
+                    className={inputClasses.join(" ")}
+                    value={props.value}
+                    onChange={props.changed}
+                >
+                    {props.elementConfig.options.map((option) => (
+                        <option key={option.value} value={option.value}>
+                            {option.displayValue}
+                        </option>
+                    ))}
+                </select>
             );
             break;
         default:
-            inputElement = <input className={styles.InputElement} {...props} />;
+            inputElement = (
+                <input
+                    className={inputClasses.join(" ")}
+                    {...props.elementConfig}
+                    value={props.value}
+                    onChange={props.changed}
+                />
+            );
     }
+
     return (
-        <div className={styles.Input}>
-            <label className={styles.Label}>{props.label}</label>
+        <div className={classes.Input}>
+            <label className={classes.Label}>{props.label}</label>
             {inputElement}
         </div>
     );
